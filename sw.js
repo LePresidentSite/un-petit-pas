3fe113c3bf58b4c821deb231f8a80c82c4742fd7
avant-la-paie <caro.lemay@hotmail.ca>
2026-09-07 02:01:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const CACHE_VERSION = "un-petit-pas-v95";
+const CACHE_VERSION = "un-petit-pas-v96";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=20260907-radio-player-draggable-v1",
   "./config.js",
   "./account.js?v=20260619-android-sync-v1",
-  "./data.js?v=20260907-zones-qc-v2",
+  "./data.js?v=20260907-zones-qc-v3",
   "./db.js?v=20260829-gamification-phase3",
   "./app.js?v=20260907-radio-player-draggable-v1",
   "./assets/caroline-portrait-v2.jpg?v=20260611",
