1985b734a9683f1f468e54216867dd8cfabfa373
avant-la-paie <caro.lemay@hotmail.ca>
2026-09-09 22:35:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const CACHE_VERSION = "un-petit-pas-v102";
+const CACHE_VERSION = "un-petit-pas-v103";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260910-today-collection-blue-v1",
+  "./styles.css?v=20260910-today-collection-blue-v2",
   "./config.js",
   "./account.js?v=20260619-android-sync-v1",
   "./data.js?v=20260910-nouvelles-collections-v1",
